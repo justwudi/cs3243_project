964828c3f5acac38ab84672d3f38f54330179ae6
Wu Di <justwudi@gmail.com>
2016-03-21 16:59:31 +0800

    Partial implementation of feature methods

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,30 +1,53 @@
+import java.util.*;
 
 public class PlayerSkeleton {
 
 	// Array of 10, each index corresponds to the column's height
 	private int[] getHeight(State state) {
-		return int[];
+		return state.getTop();
 	}
 
 	// Array of 9, each index corresponds to the difference between that column
 	// and the next column
 	private int[] getAbsoluteDifference(State state) {
-		return int[];
+		int[] differenceArray = new int[state.COLS - 1];
+
+		int[] heightArray = getHeight(state);
+
+		for (int col = 0; col < differenceArray.length; col++) {
+			differenceArray[col] = Math.abs(heightArray[col] - heightArray[col + 1]);
+		}
+
+		return differenceArray;
 	}
 
 	// Height of the tallest column
 	private int getMaxColumnHeight(State state) {
-		return 0;
+		int max = 0;
+		int[] heightArray = getHeight(state);
+
+		for (int col = 0; col < heightArray.length; col++) {
+			int currentHeight = heightArray[col];
+			if (currentHeight > max) {
+				max = currentHeight;
+			}
+		}
+
+		return max;
 	}
 
 	// Self explanatory
 	private int numberOfHoles(State state) {
+		int[] heightArray = getHeight(state);
+		int[][] field = state.getField();
+		int col;
+		int row;
+
 		return 0;
 	}
 
 	//implement this function to have a working system
 	public int pickMove(State state, int[][] legalMoves) {
-
 		return 0;
 	}
 
