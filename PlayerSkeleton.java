cb7d405c51c0ede6f582d8000a35f9a6109ca79b
Wu Di <justwudi@gmail.com>
2016-03-21 18:47:30 +0800

    Implement function to generate next field

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -2,17 +2,91 @@ import java.util.*;
 
 public class PlayerSkeleton {
 
+	private int[][] nextField;
+	private int[] heightArray;
+
+	private void generateNextField(State state, int[] move) {
+		nextField = deepCopyField(state.getField());
+		heightArray = state.getTop();
+		int nextPieceIndex = state.getNextPiece();
+		int turn = state.getTurnNumber();
+
+		int orientIndex = move[State.ORIENT];
+		int startColumn = move[State.SLOT];
+
+		// piece information
+		int[] pTop = state.getpTop()[nextPieceIndex][orientIndex];
+		int[] pBottom = state.getpBottom()[nextPieceIndex][orientIndex];
+		int pWidth = state.getpWidth()[nextPieceIndex][orientIndex];
+		int pHeight = state.getpHeight()[nextPieceIndex][orientIndex];
+
+		// height if the first column makes contact
+		int height = heightArray[startColumn] - pBottom[0];
+		int nextColumn;
+		// for each column beyond the first in the piece
+		for (int col = 1; col < pWidth; col++) {
+			nextColumn = startColumn + col;
+			height = Math.max(height, heightArray[nextColumn] - pBottom[col]);
+		}
+
+		// for each column in the piece - fill in the appropriate blocks
+		for(int col = 0; col < pWidth; col++) {
+			int startRow = height + pBottom[col];
+			int endRow = height + pTop[col];
+
+			// from bottom to top of brick
+			for (int row = startRow; row < endRow; row++) {
+				nextField[row][startColumn + col] = turn;
+			}
+		}
+
+		// adjust top
+		for (int col = 0; col < pWidth; col++) {
+			heightArray[startColumn + col] = height + pTop[col];
+		}
+
+		int rowsCleared = 0;
+
+		// check for full rows - starting at the top
+		for (int row = height + pHeight - 1; row >= height; row--) {
+			// check all columns in the row
+			boolean full = true;
+			for (int col = 0; col < State.COLS; col++) {
+				if (nextField[row][col] == 0) {
+					full = false;
+					break;
+				}
+			}
+
+			// if the row was full - remove it and slide above stuff down
+			if (full) {
+				rowsCleared++;
+				// for each column
+				for (int col = 0; col < State.COLS; col++) {
+
+					// slide down all bricks
+					for(int r = row; r < heightArray[col]; r++) {
+						nextField[r][col] = nextField[r+1][col];
+					}
+					// lower the top
+					heightArray[col]--;
+					while (heightArray[col] >= 1 && nextField[heightArray[col] - 1][col] == 0) {
+						heightArray[col]--;
+					}
+				}
+			}
+		}
+	}
+
 	// Array of 10, each index corresponds to the column's height
-	private int[] getHeight(State state) {
-		return state.getTop();
+	private int[] getHeight() {
+		return heightArray;
 	}
 
 	// Array of 9, each index corresponds to the difference between that column
 	// and the next column
-	private int[] getAbsoluteDifference(State state) {
-		int[] differenceArray = new int[state.COLS - 1];
-
-		int[] heightArray = getHeight(state);
+	private int[] getAbsoluteDifference() {
+		int[] differenceArray = new int[State.COLS - 1];
 
 		for (int col = 0; col < differenceArray.length; col++) {
 			differenceArray[col] = Math.abs(heightArray[col] - heightArray[col + 1]);
@@ -22,9 +96,8 @@ public class PlayerSkeleton {
 	}
 
 	// Height of the tallest column
-	private int getMaxColumnHeight(State state) {
+	private int getMaxColumnHeight() {
 		int max = 0;
-		int[] heightArray = getHeight(state);
 
 		for (int col = 0; col < heightArray.length; col++) {
 			int currentHeight = heightArray[col];
@@ -37,12 +110,7 @@ public class PlayerSkeleton {
 	}
 
 	// Self explanatory
-	private int numberOfHoles(State state) {
-		int[] heightArray = getHeight(state);
-		int[][] field = state.getField();
-		int col;
-		int row;
-
+	private int numberOfHoles() {
 		return 0;
 	}
 
@@ -68,4 +136,15 @@ public class PlayerSkeleton {
 		System.out.println("You have completed "+state.getRowsCleared()+" rows.");
 	}
 
+	public static int[][] deepCopyField(int[][] field) {
+		if (field == null) {
+				return null;
+		}
+
+		final int[][] result = new int[field.length][];
+		for (int i = 0; i < field.length; i++) {
+				result[i] = Arrays.copyOf(field[i], field[i].length);
+		}
+		return result;
+}
 }
