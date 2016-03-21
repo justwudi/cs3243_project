d9f32fd289495ad3658d1409801d23cbd3ef8551
Wu Di <justwudi@gmail.com>
2016-03-21 16:37:04 +0800

    Add method signature for features

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,8 +1,29 @@
 
 public class PlayerSkeleton {
 
+	// Array of 10, each index corresponds to the column's height
+	private int[] getHeight(State state) {
+		return int[];
+	}
+
+	// Array of 9, each index corresponds to the difference between that column
+	// and the next column
+	private int[] getAbsoluteDifference(State state) {
+		return int[];
+	}
+
+	// Height of the tallest column
+	private int getMaxColumnHeight(State state) {
+		return 0;
+	}
+
+	// Self explanatory
+	private int numberOfHoles(State state) {
+		return 0;
+	}
+
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
+	public int pickMove(State state, int[][] legalMoves) {
 
 		return 0;
 	}
