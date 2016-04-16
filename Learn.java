9160cc04d2af8ab971c6e9490ab7a63b4c9b6a5b
Wu Di <justwudi@gmail.com>
2016-04-16 18:59:47 +0800

    Add multithreading

--- Learn.java
+++ Learn.java
@@ -3,22 +3,29 @@ import java.util.concurrent.TimeUnit;
 public abstract class Learn {
   protected final int totalFeatures = 22;
   protected Weight[] weightPermutations;
-  private final int attempt = 3;
+  protected final int attempt = 3;
+  private int currentIndex = 0;
+
+  private synchronized Weight getWeight() {
+    if (currentIndex < weightPermutations.length) {
+      return weightPermutations[currentIndex++];
+    }
+    return null;
+  }
 
   protected void computeScores() {
-    for (int i = 0; i < weightPermutations.length; i++) {
-      Weight w = weightPermutations[i];
-      w.score = 0;
-      for (int a = 0; a < attempt; a++) {
-        State state = new State();
-        PlayerSkeleton p = new PlayerSkeleton();
-        p.initWeights(w);
-        while (!state.hasLost()) {
-          state.makeMove(p.pickMove(state, state.legalMoves()));
-        }
-        w.score += state.getRowsCleared();
+    currentIndex = 0;
+    int processors = Runtime.getRuntime().availableProcessors();
+    WeightComputer[] threads = new WeightComputer[processors];
+    for (int i = 0; i < processors; i++) {
+      threads[i] = new WeightComputer(this);
+      threads[i].start();
+    }
+    try {
+      for (int i = 0; i < processors; i++) {
+        threads[i].join();
       }
-      w.score /= attempt;
+    } catch (Exception e) {
     }
   }
 
@@ -38,5 +45,44 @@ public abstract class Learn {
       TimeUnit.MILLISECONDS.toSeconds(millis) -
       TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
     ));
+  }
+
+  private class TetrisState extends State {}
+
+  public class WeightComputer extends Thread {
+    Learn l;
+
+    public WeightComputer(Learn l) {
+      this.l = l;
+    }
+
+    public void run() {
+      Weight w;
+      TetrisState state;
+      PlayerSkeleton p;
+
+      while (true) {
+        synchronized (this) {
+          w = l.getWeight();
+        }
+
+        if (w == null) {
+          break;
+        }
+
+        w.score = 0;
+        for (int a = 0; a < l.attempt; a++) {
+          state = new TetrisState();
+          p = new PlayerSkeleton();
+          p.initWeights(w);
+
+          while (!state.hasLost()) {
+            state.makeMove(p.pickMove(state, state.legalMoves()));
+          }
+          w.score += state.getRowsCleared();
+        }
+        w.score /= l.attempt;
+      }
+    }
   }
 }
